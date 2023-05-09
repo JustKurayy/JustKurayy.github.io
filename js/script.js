088f1979a5c7cbce6ff6e85437075c13474dfae6
SirQuandale <302685723@student.rocmondriaan.nl>
2023-05-09 12:57:50 +0200

    os icons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const aboutMe = document.querySelector('.about-me');
 const tab = document.querySelector('.tab');
+const closeItem = document.querySelector('.close-item');
+const maxItem = document.querySelector('.max-item');
+const minItem = document.querySelector('.min-item');
 
 aboutMe.addEventListener('click', () => {
-    
+    document.querySelector('.tab').classList.remove('invisible');
+    document.querySelector('.tab-info').textContent = "About me";
+});
+
+closeItem.addEventListener('click', () => {
+    document.querySelector('.tab').classList.add('invisible');
 });
