a30f53cbe1ff7461041a11e7073713aa3beb9c91
SirQuandale <302685723@student.rocmondriaan.nl>
2023-06-13 12:59:17 +0200

    setinterval name changer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 let title = document.querySelector('.title-names').textContent;
 let allTitles = ["Software Developer", "Game Designer", "Game Director", "Writer"];
+let current = 0;
 
 function changeTitle() {
-    title = `${allTitles[Math.floor(Math.random() * 3)]}`;
+    if (current < allTitles.length) {
+        document.querySelector('.title-names').textContent = allTitles[current];
+    } else {
+        current = 0;
+        document.querySelector('.title-names').textContent = allTitles[current];
+    }
+    current++;
 }
 
-setInterval(changeTitle, 1000);
+setInterval(changeTitle, 2000);
+
+document.querySelector('.higher-lower').addEventListener('click', () => {
+    window.open("https://sirquandale.github.io/Higher-Lower", _blank);
+});
